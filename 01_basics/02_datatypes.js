b9f1c59eb04af7f7040e991282b72852cfe202d0
Chinmayaagrawal17 <141646747+Chinmayaagrawal17@users.noreply.github.com>
2023-10-01 08:22:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+ "use strict";// treat all js coede as newer version
+
+//  alert(3+3) // we are using node.js , not brower
+
+console.log(3+3);
+
+console.log("hitesh");
+
+let name="chinmaya"// 1. string datatype
+let age="20"// 2.number datatype
+let isLoggedIn=false//3.bollean datatype
+let state=null //4.null
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof"hitesh");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
